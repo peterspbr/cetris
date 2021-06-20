dd7cd9b1c4d3640bf2f0576c38793dd1d034c4d6
peterspbr <m4sh1lo@protonmail.com>
2021-06-20 13:37:56 -0300

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,11 @@ using namespace std;
 #define width  800
 #define height 600
 
+
+// Global variables
 struct Point {int x, y;} a[4], b[4]; // Tetrominos axis
 
+int score;
 const int fW = 10;
 const int fH = 20;
 
@@ -24,6 +27,7 @@ int coords[7][4] = {
     2, 3, 4, 5
 };
 
+// Check for collision
 bool check()
 {
     for(int i = 0; i < 4; i++)
@@ -37,37 +41,46 @@ bool check()
 
 int main()
 {
+    // Seed for random
     srand(time(NULL));
 
+    // SFML parameters
     sf::RenderWindow window(sf::VideoMode(width, height), "Cetris");
     window.setFramerateLimit(30);
 
+    // Create textures
     sf::Texture tTexture;
     tTexture.loadFromFile("images/tetrominos.png");
     sf::Texture bgTexture;
     bgTexture.loadFromFile("images/bg.jpg");
 
+    // Create sprites
     sf::Sprite tetrominos;
     tetrominos.setTexture(tTexture);
     sf::Sprite bg;
     bg.setTexture(bgTexture);
 
-    int xPos = 0, p = 1;
+    int xPos = 0, p = 0; // X position and tetromino number
     
+    // Clock variables
     float timer = 0, delay = 0.8;
 
+    // Rotate
     bool rotate = false;
 
     sf::Clock clock;
 
+    // Game loop 
     while(window.isOpen())
     {
+        // Set the clock
         float time = clock.getElapsedTime().asSeconds();
         clock.restart();
         timer += time;
 
         sf::Event event;
 
+        // Poll keyboard events
         while(window.pollEvent(event))
         {   
             if(event.type == sf::Event::Closed)
@@ -96,12 +109,14 @@ int main()
             }
         }
 
+        // Movement in X axis
         for(int i = 0; i < 4; i++)
         {
             b[i] = a[i];
             a[i].x += xPos;
         }
 
+        // Collision detection
         if(!check())
         {
             for(int i = 0; i < 4; i++)
@@ -110,6 +125,7 @@ int main()
             }
         }
 
+        // Rotate the tetromino
         if(rotate)
         {
             Point point = a[1];
@@ -123,6 +139,7 @@ int main()
                 a[i].y = point.y + y;
             }
 
+            // If is colliding rotation will be disabled
             if(!check())
             {
                 for(int i = 0; i < 4; i++)
@@ -132,6 +149,7 @@ int main()
             }
         }
 
+        // Clock
         if(timer > delay)
         {
             for(int i = 0; i < 4; i++)
@@ -147,8 +165,8 @@ int main()
                     field[b[i].y][b[i].x] = p;
                 }
 
-                p = 1 + rand()%7;
-                int n = rand()%7;
+                p = 1 + rand() % 7;
+                int n = rand() % 7;
 
                 for(int i = 0; i < 4; i++)
                 {
@@ -160,17 +178,20 @@ int main()
             timer = 0;
         }
 
+        // Check for lines
         int k = fH - 1;
 
         for (int i = fH - 1; i > 0; i--)
         {
             int count = 0;
+            
             for (int j = 0; j < fW; j++)
             {
                 if (field[i][j])
                 {
                     count++;
                 }
+                
                 field[k][j] = field[i][j];
             }
 
@@ -180,13 +201,18 @@ int main()
             }
         }
 
+        // Clear variables to render the next tetromino
         xPos = 0;
         rotate = false;
         delay = 0.8;
 
+        // Clear the frames with a black background color
         window.clear(sf::Color::Black);
+
+        // Draw the background texture
         window.draw(bg);
 
+        // Draw tetrominos
         for(int i = 0; i < fH; i++)
         {
             for(int j = 0; j < fW; j++)
@@ -205,6 +231,7 @@ int main()
             window.draw(tetrominos);
         }
 
+        // Render everything
         window.display();
     }
 
